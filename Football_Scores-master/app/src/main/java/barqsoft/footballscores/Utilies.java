4985d277c44c8bf461df15fb8f48f84a58d8d62f
Ishan Khanna <ishan1604@gmail.com>
2015-10-23 18:33:07 +0530

    All strings made translatable

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utilies.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utilies.java
@@ -1,5 +1,8 @@
 package barqsoft.footballscores;
 
+import android.content.Context;
+import android.content.res.Resources;
+
 /**
  * Created by yehya khaled on 3/3/2015.
  */
@@ -22,34 +25,35 @@ public class Utilies
             default: return R.string.league_not_known;
         }
     }
-    public static String getMatchDay(int match_day,int league_num)
+    public static String getMatchDay(Context context, int match_day,int league_num)
     {
+        Resources res = context.getResources();
         if(league_num == CHAMPIONS_LEAGUE)
         {
             if (match_day <= 6)
             {
-                return "Group Stages, Matchday : 6";
+                return res.getString(R.string.group_stage_text) + ", " + res.getString(R.string.matchday_text) + " : 6";
             }
             else if(match_day == 7 || match_day == 8)
             {
-                return "First Knockout round";
+                return res.getString(R.string.first_knockout_round);
             }
             else if(match_day == 9 || match_day == 10)
             {
-                return "QuarterFinal";
+                return res.getString(R.string.quarter_final);
             }
             else if(match_day == 11 || match_day == 12)
             {
-                return "SemiFinal";
+                return res.getString(R.string.semi_final);
             }
             else
             {
-                return "Final";
+                return res.getString(R.string.final_text);
             }
         }
         else
         {
-            return "Matchday : " + String.valueOf(match_day);
+            return res.getString(R.string.matchday_text) + " : " + String.valueOf(match_day);
         }
     }
 
